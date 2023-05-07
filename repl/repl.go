3455578686544fe03a153998b49fe3e1964d36f5
Seng <engelhardt_stefan@gmx.de>
2023-05-07 10:18:45 +0200

    added repl and main

---
+++ repl/repl.go
@@ -0,0 +1,32 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprint(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		lexer := lexer.New(line)
+
+		tok := lexer.NextToken()
+		for tok.Type != token.EOF {
+			fmt.Fprintf(out, "%+v\n", tok)
+			tok = lexer.NextToken()
+		}
+	}
+}
