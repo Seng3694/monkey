ec0765ab9bb0f5553dc92b46a042e91492613395
Seng <engelhardt_stefan@gmx.de>
2023-05-09 18:59:51 +0200

    added String function for easier debugging and testing

---
+++ ast/ast_test.go
@@ -0,0 +1,31 @@
+package ast
+
+import (
+	"monkey/token"
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENTIFIER, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENTIFIER, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	expected := "let myVar = anotherVar;"
+	actual := program.String()
+
+	if expected != actual {
+		t.Errorf("program.String() wrong. expected='%q', got='%q'", expected, actual)
+	}
+}
