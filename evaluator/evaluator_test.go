44a4b113f86af3eb243865684b4f2b9571c00a8a
Seng <engelhardt_stefan@gmx.de>
2023-05-17 20:01:46 +0200

    added bang prefix expression evaluation

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -37,6 +37,25 @@ func TestEvalBooleanExpressions(t *testing.T) {
 	}
 }
 
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
 func testEval(input string) object.Object {
 	lexer := lexer.New(input)
 	parser := parser.New(lexer)
