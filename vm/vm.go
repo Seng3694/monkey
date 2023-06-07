5d61cb7cb43bee11bf7943267f0e6d16199c23fc
Seng <engelhardt_stefan@gmx.de>
2023-06-07 19:51:04 +0200

    added vm support for conditionals

--- vm/vm.go
+++ vm/vm.go
@@ -79,6 +79,16 @@ func (vm *VM) Run() error {
 			if err != nil {
 				return err
 			}
+		case code.OpJump:
+			pos := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip = pos - 1
+		case code.OpJumpNotTruthy:
+			pos := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip += 2
+			condition := vm.pop()
+			if !isTruthy(condition) {
+				ip = pos - 1
+			}
 		case code.OpPop:
 			vm.pop()
 		}
@@ -217,4 +227,13 @@ func nativeBoolToBooleanObject(input bool) *object.Boolean {
 		return True
 	}
 	return False
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj := obj.(type) {
+	case *object.Boolean:
+		return obj.Value
+	default:
+		return true
+	}
 }
