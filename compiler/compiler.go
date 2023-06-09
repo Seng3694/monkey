0ffa4462585b5b66440267f01d26cdf4255bcb11
Seng <engelhardt_stefan@gmx.de>
2023-06-09 13:36:37 +0200

    added string literals and concatenation

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -115,6 +115,10 @@ func (c *Compiler) Compile(node ast.Node) error {
 			c.emit(code.OpFalse)
 		}
 
+	case *ast.StringLiteral:
+		str := &object.String{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(str))
+
 	case *ast.IfExpression:
 		err := c.Compile(node.Condition)
 		if err != nil {
