963cfa5d73064b0532778d9cfff011077077b466
Seng <engelhardt_stefan@gmx.de>
2023-06-18 22:13:47 +0200

    added vm support for function calls

--- vm/vm.go
+++ vm/vm.go
@@ -178,6 +178,23 @@ func (vm *VM) Run() error {
 				return err
 			}
 
+		case code.OpCall:
+			fn, ok := vm.stack[vm.sp-1].(*object.CompiledFunction)
+			if !ok {
+				return fmt.Errorf("calling non-function")
+			}
+			frame := NewFrame(fn)
+			vm.pushFrame(frame)
+
+		case code.OpReturnValue:
+			returnValue := vm.pop()
+			vm.popFrame()
+			vm.pop()
+			err := vm.push(returnValue)
+			if err != nil {
+				return err
+			}
+
 		case code.OpPop:
 			vm.pop()
 		}
