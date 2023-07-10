59a6389bf1163560b3b8d652ac2b308b0701137b
Seng <engelhardt_stefan@gmx.de>
2023-07-10 19:00:37 +0200

    added additional vm test

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -210,6 +210,16 @@ func TestFirstClassFunctions(t *testing.T) {
 			returnsOneReturner()();`,
 			expected: 1,
 		},
+		{
+			input: `
+			let returnsOneReturner = fn() {
+				let returnsOne = fn() { 1; };
+				returnsOne;
+			};
+			returnsOneReturner()();
+			`,
+			expected: 1,
+		},
 	}
 	runVmTests(t, tests)
 }
