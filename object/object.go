73792b29583ccbe184379c3a238516fae820b079
Seng <engelhardt_stefan@gmx.de>
2023-07-10 18:57:52 +0200

    added vm support for scoped local variables

--- object/object.go
+++ object/object.go
@@ -106,6 +106,7 @@ func (b *Builtin) Inspect() string  { return "builtin function" }
 
 type CompiledFunction struct {
 	Instructions code.Instructions
+	NumLocals    int
 }
 
 func (cf *CompiledFunction) Type() ObjectType { return COMPILED_FUNCTION_OBJ }
--- vm/vm.go
+++ vm/vm.go
@@ -33,7 +33,7 @@ type VM struct {
 
 func New(bytecode *compiler.ByteCode) *VM {
 	mainFn := &object.CompiledFunction{Instructions: bytecode.Instructions}
-	mainFrame := NewFrame(mainFn)
+	mainFrame := NewFrame(mainFn, 0)
 
 	frames := make([]*Frame, MaxFrames)
 	frames[0] = mainFrame
@@ -142,6 +142,25 @@ func (vm *VM) Run() error {
 
 			vm.globals[globalIndex] = vm.pop()
 
+		case code.OpGetLocal:
+			localIndex := code.ReadUint8(instr[ip+1:])
+			vm.currentFrame().ip += 1
+
+			frame := vm.currentFrame()
+
+			err := vm.push(vm.stack[frame.basePointer+int(localIndex)])
+			if err != nil {
+				return err
+			}
+
+		case code.OpSetLocal:
+			localIndex := code.ReadUint8(instr[ip+1:])
+			vm.currentFrame().ip += 1
+
+			frame := vm.currentFrame()
+
+			vm.stack[frame.basePointer+int(localIndex)] = vm.pop()
+
 		case code.OpArray:
 			numElements := int(code.ReadUint16(instr[ip+1:]))
 			vm.currentFrame().ip += 2
@@ -183,13 +202,15 @@ func (vm *VM) Run() error {
 			if !ok {
 				return fmt.Errorf("calling non-function")
 			}
-			frame := NewFrame(fn)
+			frame := NewFrame(fn, vm.sp)
 			vm.pushFrame(frame)
+			vm.sp = frame.basePointer + fn.NumLocals
 
 		case code.OpReturnValue:
 			returnValue := vm.pop()
-			vm.popFrame()
-			vm.pop()
+
+			frame := vm.popFrame()
+			vm.sp = frame.basePointer - 1
 
 			err := vm.push(returnValue)
 			if err != nil {
@@ -197,8 +218,8 @@ func (vm *VM) Run() error {
 			}
 
 		case code.OpReturn:
-			vm.popFrame()
-			vm.pop()
+			frame := vm.popFrame()
+			vm.sp = frame.basePointer - 1
 
 			err := vm.push(Null)
 			if err != nil {
