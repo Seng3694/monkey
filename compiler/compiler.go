08d6b57e4332427b054dea3365648099e82ddbb5
Seng <engelhardt_stefan@gmx.de>
2023-07-11 19:02:57 +0200

    added compilation of function arguments

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -249,6 +249,10 @@ func (c *Compiler) Compile(node ast.Node) error {
 	case *ast.FunctionLiteral:
 		c.enterScope()
 
+		for _, p := range node.Parameters {
+			c.symbolTable.Define(p.Value)
+		}
+
 		err := c.Compile(node.Body)
 		if err != nil {
 			return err
@@ -275,7 +279,15 @@ func (c *Compiler) Compile(node ast.Node) error {
 		if err != nil {
 			return nil
 		}
-		c.emit(code.OpCall)
+
+		for _, a := range node.Arguments {
+			err := c.Compile(a)
+			if err != nil {
+				return err
+			}
+		}
+
+		c.emit(code.OpCall, len(node.Arguments))
 
 	case *ast.ReturnStatement:
 		err := c.Compile(node.ReturnValue)
